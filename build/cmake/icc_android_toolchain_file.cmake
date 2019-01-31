0e6e02dc8cadd2099575b9a8a72a1537211b7b10
Krzysztof Bruniecki <krzysztof.bruniecki@intel.com>
2019-01-31 14:22:50 +0100

    task-kbruniec-2019-01-30-fix-icc-quickbuild-pie-issue: Add -pie, -fPIE and -fPIC flags for Android ICC build

--- build/cmake/icc_android_toolchain_file.cmake
+++ build/cmake/icc_android_toolchain_file.cmake
@@ -42,7 +42,8 @@ set(CMAKE_SYSTEM_PROCESSOR x86_64)
 set(CMAKE_SYSROOT ${CMAKE_ANDROID_STANDALONE_TOOLCHAIN}/sysroot)
 include_directories("${CMAKE_ANDROID_STANDALONE_TOOLCHAIN}/include/c++/4.9.x")
 # Needed for building executables including CMake simple C++ test program
-set(CMAKE_EXE_LINKER_FLAGS_INIT "-L${CMAKE_ANDROID_STANDALONE_TOOLCHAIN}/x86_64-linux-android/lib64 -lc++_shared")
+set(CMAKE_EXE_LINKER_FLAGS_INIT "-L${CMAKE_ANDROID_STANDALONE_TOOLCHAIN}/x86_64-linux-android/lib64 -lc++_shared -fPIE -pie")
+set(EXTRA_EXE_COMPILE_OPTIONS "-fPIE")
 
 set(CMAKE_C_COMPILER ${ICC_DIR}/icc)
 set(CMAKE_CXX_COMPILER ${ICC_DIR}/icpc)
