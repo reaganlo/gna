f4870e66b0527244736e440bbaf578b12df5b43f
Krzysztof Bruniecki <krzysztof.bruniecki@intel.com>
2020-02-10 17:34:05 +0100

    task-kb-200210-fix-error-142-on-dll-load: Disable most optimizations

--- build/cmake/gna_compile_flags.cmake
+++ build/cmake/gna_compile_flags.cmake
@@ -42,13 +42,13 @@ if(${CMAKE_SYSTEM_NAME} STREQUAL "Windows")
 
   # Debug/Release Multithreaded libraries
   set(GNA_COMPILE_FLAGS_DEBUG ${GNA_COMPILE_FLAGS_DEBUG} /MDd /Od /RTC1)
-  set(GNA_COMPILE_FLAGS_RELEASE ${GNA_COMPILE_FLAGS_RELEASE} /MD /Oi /Gy)
+  set(GNA_COMPILE_FLAGS_RELEASE ${GNA_COMPILE_FLAGS_RELEASE} /MD /Od /Oi)
 
   # Optimization level
   if(${CMAKE_CXX_COMPILER_ID} STREQUAL "Intel")
-    set(GNA_COMPILE_FLAGS_RELEASE ${GNA_COMPILE_FLAGS_RELEASE} /O3 /Qinline-forceinline)
+    set(GNA_COMPILE_FLAGS_RELEASE ${GNA_COMPILE_FLAGS_RELEASE} /Qinline-forceinline)
   elseif(${CMAKE_CXX_COMPILER_ID} STREQUAL MSVC)
-    set(GNA_COMPILE_FLAGS_RELEASE ${GNA_COMPILE_FLAGS_RELEASE} /O2)
+    set(GNA_COMPILE_FLAGS_RELEASE ${GNA_COMPILE_FLAGS_RELEASE})
   endif()
 
   # Linker options
@@ -97,7 +97,7 @@ else()
   set(GNA_COMPILE_FLAGS_DEBUG ${GNA_COMPILE_FLAGS_DEBUG}
       -g -O0)
   set(GNA_COMPILE_FLAGS_RELEASE ${GNA_COMPILE_FLAGS_RELEASE}
-      -fvisibility=hidden -fstack-protector-all -O3)
+      -fvisibility=hidden -fstack-protector-all -O0)
 
   # ICC intrinsics inline expansion
   if(${CMAKE_CXX_COMPILER_ID} STREQUAL "Intel")
